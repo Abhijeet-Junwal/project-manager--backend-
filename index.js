8e74b668b6556e5670694382c696acdb1836eb7f
Abhijeet-Junwal <abhijeetjunwal@gmail.com>
2025-12-24 14:09:33 +0530

    added nodemon and set up .env

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Let's Starttttt.......")
+import dotenv from "dotenv";
+
+dotenv.config({
+    path : './.env',
+});
+
+let myusername = process.env.user;
+console.log("user : " + myusername);
+
+let mydatabase = process.env.database;
+console.log("database : " , mydatabase);
+
+
+
+console.log("Start of the Backend project")
