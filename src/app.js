d481ed57e104c8dc5684e5979eb76d1ae27ddfcb
Abhijeet-Junwal <abhijeetjunwal@gmail.com>
2025-12-25 15:37:45 +0530

    Express configuration(app.use) , COSR integration and setting up ApiResponse and ApiError

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`Welcome Welcome.....`)
-})
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({limit: "16kb", extended: "true"}));
+
+app.use(express.static("public"));
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.get('/', (req, res) => {
     res.send("Hello World!!")
