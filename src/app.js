7c07644d7f54b4840f593977c1d1a71eb33dce94
Abhijeet-Junwal <abhijeetjunwal@gmail.com>
2025-12-29 16:08:10 +0530

    Heath Check API

--- src/app.js
+++ src/app.js
@@ -3,17 +3,24 @@ import cors from 'cors';
 
 const app = express();
 
+//basic configurations
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit: "16kb", extended: "true"}));
 
 app.use(express.static("public"));
 
+//cors configurations
 app.use(cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+//import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get('/', (req, res) => {
     res.send("Hello World!!")
