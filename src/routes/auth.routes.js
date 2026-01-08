54c190a5cf16f82792096d00350f8c19ea92d129
Abhijeet-Junwal <abhijeetjunwal@gmail.com>
2026-01-08 16:31:27 +0530

    User Logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { registerUser, loggedinUser } from "../controllers/auth.controllers.js";
+import { registerUser, loggedinUser, logoutUser } from "../controllers/auth.controllers.js";
 import { userRegistorValidator, userLoginValidator } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegistorValidator(), validate, registerUser);
 
 router.route("/login").post(userLoginValidator(), validate, loggedinUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);
  
 
 export default router;
